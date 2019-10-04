4346f6d4a31981068c1bebc216262f258e029279
GCodeOn <gerardosoto93@gmail.com>
2019-10-03 23:25:54 -0700

    logo loader + ring + fill animation

--- resources/assets/js/components/LogoLoader.vue
+++ resources/assets/js/components/LogoLoader.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="logo-loader">
+        <logo-svg class="ring"></logo-svg>
         <logo-line-draw class="logo"></logo-line-draw>
         <div class="bottom-loader">
         </div>
@@ -11,45 +12,7 @@
     }
 </script>
 
-<style lang="scss">
-
-.logo-loader {
-    width                     : 100vw;
-    height                    : 100vh;
-    position                  : fixed;
-    z-index                   : 10000000;
-    left                      : 0px;
-    top                       : 0px;
-    background                : #323334;
-    pointer-events            : none;
-    animation                 : opacity-out 4s;
-    animation-fill-mode       : forwards;
-    animation-iteration-count : 1;  
-    
-    .logo {
-        width     : 300px;
-        transform : translate(-50%, -50%);
-        left      : 50%;
-        top       : 50%;
-        position  : absolute;
-        display   : block;
-        animation : fade 3s;
-        filter    : drop-shadow(0px 2px 16px rgba(0, 0, 0, 0.2));
-    }
-
-    .bottom-loader {
-        width                     : 0%;
-        position                  : absolute;
-        bottom                    : 0px;
-        height                    : 7px;
-        animation-delay           : 1s;
-        background                : get-color(primary);
-        animation                 : loader-increase 4s ease-in-out;
-        animation-fill-mode       : forwards;
-        animation-iteration-count : 1;
-    }
-
-}
+<style lang="scss" scoped>
 
 @keyframes loader-increase {
     100% {
@@ -86,4 +49,51 @@
     }
 }
 
+.logo-loader {
+    width                     : 100vw;
+    height                    : 100vh;
+    position                  : fixed;
+    z-index                   : 10000000;
+    left                      : 0px;
+    top                       : 0px;
+    background                : #323334;
+    pointer-events            : none;
+    animation                 : opacity-out 4s;
+    animation-fill-mode       : forwards;
+    animation-iteration-count : 1;  
+    
+    .logo {
+        width     : 300px;
+        transform : translate(-50%, -50%);
+        left      : 50%;
+        top       : 50%;
+        position  : absolute;
+        display   : block;
+        animation : fade 3s;
+        filter    : drop-shadow(0px 2px 16px rgba(0, 0, 0, 0.2));
+    }
+    .ring {
+        width     : 400px;
+        transform : translate(-50%, -50%);
+        left      : 50%;
+        top       : 50%;
+        position  : absolute;
+        display   : block;
+        animation : fade 3s;
+    }
+
+
+    .bottom-loader {
+        width                     : 0%;
+        position                  : absolute;
+        bottom                    : 0px;
+        height                    : 7px;
+        animation-delay           : 1s;
+        background                : get-color(primary);
+        animation                 : loader-increase 4s ease-in-out;
+        animation-fill-mode       : forwards;
+        animation-iteration-count : 1;
+    }
+
+}
 </style>
