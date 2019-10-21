70f5f05d50f4c50b8e1229623be3f580eef4b0d1
GCodeOn <gerardosoto93@gmail.com>
2019-10-20 17:17:58 -0700

    added metas

--- resources/views/partials/metas.blade.php
+++ resources/views/partials/metas.blade.php
@@ -12,6 +12,6 @@
 <meta property="og:url" content="{{ env('APP_URL')  }}"/>
 <meta property="og:type" content="website"/>
 <meta property="og:title" content="Gerardo Soto"/>
-<meta property="og:description" content="Gerardo Soto gsoto@gcodeon.me"/>
-<meta property="og:image" content=""/>
+<meta property="og:description" content="Gerardo Soto | Software Developer"/>
+<meta property="og:image" content="{{ env('APP_URL')  }}/images/gcodon.png"/>
 <title>GCodeOn | Gera Soto</title>
